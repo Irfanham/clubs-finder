a90966a66688fbb2a88c48e74aa4dd6a977cc46f
Irfanham <irfanhamami4@gmail.com>
2025-07-11 21:39:11 +0700

    update clubs finder

--- src/script/view/home.js
+++ src/script/view/home.js
@@ -1,1 +1,68 @@
-import Utils from '../utils.js'
+import Utils from "../utils.js";
+import Clubs from "../data/local/clubs.js";
+
+const home = () => {
+    const searchFormElement = document.querySelector('#searchForm')
+  const clubListContainerElement = document.querySelector("#clubListContainer");
+  const clubQueryWaitingElement = clubListContainerElement.querySelector('.query-waiting')
+  const clubLoadingElement =
+    clubListContainerElement.querySelector(".search-loading");
+  const clubListElement = clubListContainerElement.querySelector(".club-list");
+  const listElement = clubListElement.querySelector(".list");
+
+  const showSportClub = (query) => {
+    showLoading();
+    const result = Clubs.searchClub(query);
+    displayResult(result);
+    showClubList();
+  };
+
+  const displayResult = (clubs) => {
+    const clubItems = clubs.map((club) => {
+      return `<div class="card">
+            <img class="fan-art-club" src='${club.strTeamBadge}' alt="Fan Art: ${club.strTeam}">
+            <div class= "club-info">
+                <div class="club-info__title">
+                    <h2>${club.strTeam}</h2>
+                </div>
+                <div class="club-info__description">
+                    <p>${club.strDescrioptionEN}</p>
+                </div>
+            </div>
+            </div>`;
+    });
+    listElement.innerHTML = clubItems.join("");
+  };
+
+  const showLoading = () => {
+    Array.from(clubListContainerElement.children).forEach((element) => {
+      Utils.hideELement(element);
+    });
+    Utils.showElement(clubLoadingElement);
+  };
+
+  const showClubList = () => {
+    Array.from(clubListContainerElement.children).forEach((element) =>{
+        Utils.hideELement(element)
+    })
+    Utils.showElement(clubListElement)
+  };
+
+  const showQueryWaiting = () =>{
+    Array.from(clubListContainerElement.children).forEach((element) =>{
+        Utils.hideELement(element)
+    })
+    Utils.showElement(clubQueryWaitingElement)
+  }
+
+  const onSearchHandler = (event) => {
+    event.preventDefault();
+    const query = event.target.element.name.value
+    showSportClub(query)
+  }
+
+  searchFormElement.addEventListener('submit',onSearchHandler)
+  showQueryWaiting();
+};
+
+export default home;
